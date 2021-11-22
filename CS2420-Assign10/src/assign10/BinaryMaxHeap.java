537fc19e4ba665c64f01695c3afc279fe48a5a50
sethpolevoi <74836049+sethpolevoi@users.noreply.github.com>
2021-11-21 18:22:45 -0700

    Added BinaryMaxHeap class

---
+++ CS2420-Assign10/src/assign10/BinaryMaxHeap.java
@@ -0,0 +1,54 @@
+package assign10;
+
+import java.util.NoSuchElementException;
+
+public class BinaryMaxHeap<E> implements PriorityQueue{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void add(Object item) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Object peek() throws NoSuchElementException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object extractMax() throws NoSuchElementException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Object[] toArray() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
