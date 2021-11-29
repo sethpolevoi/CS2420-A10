adad44b8af11d3019bdf314d91376ab9c8bf374c
sethpolevoi <74836049+sethpolevoi@users.noreply.github.com>
2021-11-28 23:29:06 -0700

    updated Jdocs

--- CS2420-Assign10/src/assign10/BinaryMaxHeap.java
+++ CS2420-Assign10/src/assign10/BinaryMaxHeap.java
@@ -1,38 +1,62 @@
 package assign10;
 
-import java.util.Arrays;
 import java.util.Comparator;
 import java.util.NoSuchElementException;
 
 import java.util.List;
 
+/**
+ * This interface represents the priority queue abstract data type, defining the
+ * operations and running times expected of any data structure used to implement
+ * a priority queue.
+ * 
+ * NOTE: The item with the highest priority is the "maximum" item.
+ * 
+ * @author SETH POLEVOI and MIKE PHELPS
+ *
+ * @param <E>
+ */
 public class BinaryMaxHeap<E> implements PriorityQueue<E> {
-	
+
 	private E maxHeap[];
 	private Comparator<? super E> cmp;
-	private int size;//how many elements are in the array
-	private int capacity;//how many elements could be in the array, the maximum amount
-	
-	// This constructor creates an empty BinaryMax Heap and assumes
-	//that natural ordering (comparable) will be used for comparisons.	
-	@SuppressWarnings({ "unchecked"}) 
+	private int size;// how many elements are in the array
+	private int capacity;// how many elements could be in the array, the maximum amount
+
+	/**
+	 * This constructor creates an empty BinaryMax Heap and assumes that natural
+	 * ordering (comparable) will be used for comparisons.
+	 */
+	@SuppressWarnings({ "unchecked" })
 	public BinaryMaxHeap() {
-		maxHeap = (E []) new Object[10];
+		maxHeap = (E[]) new Object[10];
 		cmp = null;
 		size = 0;
 		capacity = 10;
-		
+
 	}
-	
-	@SuppressWarnings({ "unchecked"}) 
+
+	/**
+	 * This constructor creates an empty BinaryMax Heap uses the provided comparator
+	 * for priority sorting
+	 * 
+	 * @param cmp Comparator to sort between objects E
+	 */
+	@SuppressWarnings({ "unchecked" })
 	public BinaryMaxHeap(Comparator<? super E> cmp) {
 		maxHeap = (E[]) new Object[10];
 		this.cmp = cmp;
 		size = 0;
 		capacity = 10;
 	}
-	
-	@SuppressWarnings({ "unchecked"}) 
+
+	/**
+	 * This constructor creates a BinaryMax Heap using the provided List as the
+	 * initial data entry
+	 * 
+	 * @param numList list of elements to initialize the binaryMaxHeap in
+	 */
+	@SuppressWarnings({ "unchecked" })
 	public BinaryMaxHeap(List<? extends E> numList) {
 		cmp = null;
 		maxHeap = (E[]) new Object[numList.size()];
@@ -40,8 +64,15 @@ public class BinaryMaxHeap<E> implements PriorityQueue<E> {
 		capacity = numList.size();
 		buildHeap(numList);
 	}
-	
-	@SuppressWarnings({ "unchecked"}) 
+
+	/**
+	 * This constructor creates an empty BinaryMax Heap using the provided List as
+	 * the initial data entry and the provided comparator for priority sorting
+	 * 
+	 * @param numList List of elements to initialize the binaryMaxHeap in
+	 * @param cmp     Comparator to sort between objects E
+	 */
+	@SuppressWarnings({ "unchecked" })
 	public BinaryMaxHeap(List<? extends E> numList, Comparator<? super E> cmp) {
 		this.cmp = cmp;
 		maxHeap = (E[]) new Object[numList.size()];
@@ -51,65 +82,82 @@ public class BinaryMaxHeap<E> implements PriorityQueue<E> {
 	}
 
 	/**
-	 * Adds the given item to this priority queue.
-	 * O(1) in the average case, O(log N) in the worst case
+	 * Adds the given item to this priority queue. O(1) in the average case, O(log
+	 * N) in the worst case
 	 * 
-	 * @param item
+	 * @param item Item to add to the heap
 	 */
 	@Override
 	public void add(E item) {
-		
-		//If array is too small for another item, grow it.
-				if(size+1 > capacity) {
-					growArray();
-				}
-				//Add item to last index in current array
-				maxHeap[size] = item;
-				//Percolate item to its correct position
-				percolateUp();
-				//Increment size
-				size++;
+
+		// If array is too small for another item, grow it.
+		if (size + 1 > capacity) {
+			growArray();
+		}
+		// Add item to last index in current array
+		maxHeap[size] = item;
+		// Percolate item to its correct position
+		percolateUp();
+		// Increment size
+		size++;
 	}
-	
+
+	/**
+	 * Add's each value in the given list to the heap
+	 * 
+	 * @param numList List of elements to initialize the binaryMaxHeap in
+	 */
 	private void buildHeap(List<? extends E> numList) {
-		for(E item : numList) {
+		for (E item : numList) {
 			this.add(item);
 		}
 	}
-	
+
 	private void percolateUp() {
 		int currentIndex = size;
-		//while currentIndex value is greater than its parents value, 
-		//execute switch, and update currentIndex to equal the index it was switched with
-		while(currentIndex > 0 && innerCompare(maxHeap[currentIndex], maxHeap[parentIndex(currentIndex)]) > 0) {
-			//preserve parent value
+		// while currentIndex value is greater than its parents value,
+		// execute switch, and update currentIndex to equal the index it was switched
+		// with
+		while (currentIndex > 0 && innerCompare(maxHeap[currentIndex], maxHeap[parentIndex(currentIndex)]) > 0) {
+			// preserve parent value
 			E temp = maxHeap[parentIndex(currentIndex)];
-			//copy child value to parent index
+			// copy child value to parent index
 			maxHeap[parentIndex(currentIndex)] = maxHeap[currentIndex];
-			//copy parent value to child index
+			// copy parent value to child index
 			maxHeap[currentIndex] = temp;
-			
-			//update current index so we know examine at a level higher
+
+			// update current index so we know examine at a level higher
 			currentIndex = parentIndex(currentIndex);
 		}
-		
+
 	}
-	
+
+	/**
+	 * Grows the heap storage by twice the capacity and copies over all the old
+	 * values to the new larger heap
+	 */
 	@SuppressWarnings("unchecked")
 	private void growArray() {
-		//create a new array twice the capacity as the current array
-		capacity = capacity*2;
+		// create a new array twice the capacity as the current array
+		capacity = capacity * 2;
 		E[] tempMaxHeap = (E[]) new Object[capacity];
-		
-		//copy over each element
-		for(int index = 0; index < size; index++) {
+
+		// copy over each element
+		for (int index = 0; index < size; index++) {
 			tempMaxHeap[index] = maxHeap[index];
 		}
-		
-		//switch references so larger array is the new heap
+
+		// switch references so larger array is the new heap
 		maxHeap = tempMaxHeap;
 	}
-	
+
+	/**
+	 * Returns the index of the location of a left child of a given parent index
+	 * 
+	 * @param parentIndex Index of parent value
+	 * @return Returns index value of left child or returns the same index as the
+	 *         parent, if the parent has no left child
+	 */
 	private int leftChildIndex(int parentIndex) {
 		int index = (parentIndex * 2) + 1;
 		if (index > size) {
@@ -117,7 +165,14 @@ public class BinaryMaxHeap<E> implements PriorityQueue<E> {
 		}
 		return index;
 	}
-	
+
+	/**
+	 * Returns the index of the location of a right child of a given parent index
+	 * 
+	 * @param parentIndex Index of parent value
+	 * @return Returns index value of right child or returns the same index as the
+	 *         parent, if the parent has no right child
+	 */
 	private int rightChildIndex(int parentIndex) {
 		int index = (parentIndex * 2) + 2;
 		if (index > size) {
@@ -125,69 +180,71 @@ public class BinaryMaxHeap<E> implements PriorityQueue<E> {
 		}
 		return index;
 	}
-	
+
+	/**
+	 * @param childIndex
+	 * @return
+	 */
 	private int parentIndex(int childIndex) {
 		return (childIndex - 1) / 2;
 	}
 
 	/**
-	 * Returns, but does not remove, the maximum item this priority queue.
-	 * O(1)
+	 * Returns, but does not remove, the maximum item this priority queue. O(1)
 	 * 
 	 * @return the maximum item
 	 * @throws NoSuchElementException if this priority queue is empty
 	 */
 	@Override
 	public E peek() throws NoSuchElementException {
-		//if heap is empty, throw error
-		if( isEmpty() ) {
+		// if heap is empty, throw error
+		if (isEmpty()) {
 			throw new NoSuchElementException("BinaryMaxHeap is Empty");
 		}
-		//return first item in array(largest item)
+		// return first item in array(largest item)
 		return maxHeap[0];
 	}
 
 	/**
-	 * Returns and removes the maximum item this priority queue.
-	 * O(log N)
+	 * Returns and removes the maximum item this priority queue. O(log N)
 	 * 
 	 * @return the maximum item
 	 * @throws NoSuchElementException if this priority queue is empty
 	 */
 	@Override
 	public E extractMax() throws NoSuchElementException {
-		//if heap is empty, throw error
-		if( isEmpty() ) {
+		// if heap is empty, throw error
+		if (isEmpty()) {
 			throw new NoSuchElementException("BinaryMaxHeap is Empty");
 		}
 		E maxVal = peek();
 		removeMax();
 		return maxVal;
 	}
-	
-	private boolean removeMax() {
-		//replace max with last item on last level
-		maxHeap[0] = maxHeap[size-1];
-		
-		//remove last item
+
+	private void removeMax() {
+		// replace max with last item on last level
+		maxHeap[0] = maxHeap[size - 1];
+
+		// remove last item
 		size--;
-		
-		//parent is currently the top of the heap
+
+		// parent is currently the top of the heap
 		int parentIndex = 0;
-		
-		//compare parent with left and right child
+
+		// compare parent with left and right child
 		int leftCompare = leftCompare(parentIndex);
 		int rightCompare = rightCompare(parentIndex);
 		int childsCompare = childsCompare(parentIndex);
-		
-		//swap parent until it's in the correct location
-		//while parent is less than either of its children
-		while( leftCompare < 0 || rightCompare < 0) {
-			//if left child is smaller than right child
-			if( childsCompare < 0) {
+
+		// swap parent until it's in the correct location
+		// while parent is less than either of its children
+		while (leftCompare < 0 || rightCompare < 0) {
+			// if left child is smaller than right child
+			if (childsCompare < 0) {
 				swapLeft(parentIndex);
 				parentIndex = leftChildIndex(parentIndex);
-			} else {//if right child is smaller or both childs are equal
+			} else {// if right child is smaller or both childs are equal
 				swapRight(parentIndex);
 				parentIndex = rightChildIndex(parentIndex);
 			}
@@ -195,51 +252,49 @@ public class BinaryMaxHeap<E> implements PriorityQueue<E> {
 			leftCompare = leftCompare(parentIndex);
 			rightCompare = rightCompare(parentIndex);
 		}
-		return false;
 	}
-	
+
 	/**
 	 * 
 	 * 
 	 * @param parentIndex
-	 * @return				negative if parent is less than left child,
-	 * 						positive if parent is greater than left child
+	 * @return negative if parent is less than left child, positive if parent is
+	 *         greater than left child
 	 */
 	private int leftCompare(int parentIndex) {
-		return innerCompare(maxHeap[parentIndex], maxHeap[leftChildIndex(parentIndex)] );
+		return innerCompare(maxHeap[parentIndex], maxHeap[leftChildIndex(parentIndex)]);
 	}
-	
+
 	private int rightCompare(int parentIndex) {
-		return innerCompare(maxHeap[parentIndex], maxHeap[rightChildIndex(parentIndex)] );
+		return innerCompare(maxHeap[parentIndex], maxHeap[rightChildIndex(parentIndex)]);
 	}
-	
-	private int childsCompare(int parentIndex)	{
-		return -innerCompare(maxHeap[leftChildIndex(parentIndex)], maxHeap[rightChildIndex(parentIndex)] );
+
+	private int childsCompare(int parentIndex) {
+		return -innerCompare(maxHeap[leftChildIndex(parentIndex)], maxHeap[rightChildIndex(parentIndex)]);
 	}
-	
-	private void swapLeft(int parentIndex)	{
+
+	private void swapLeft(int parentIndex) {
 		E holder = maxHeap[parentIndex];
 		maxHeap[parentIndex] = maxHeap[leftChildIndex(parentIndex)];
 		maxHeap[leftChildIndex(parentIndex)] = holder;
 	}
-	
-	private void swapRight(int parentIndex)	{
+
+	private void swapRight(int parentIndex) {
 		E holder = maxHeap[parentIndex];
 		maxHeap[parentIndex] = maxHeap[rightChildIndex(parentIndex)];
 		maxHeap[rightChildIndex(parentIndex)] = holder;
 	}
-	
+
 	@SuppressWarnings("unchecked")
 	private int innerCompare(E item1, E item2) {
 		if (cmp == null) {
-			return ((Comparable<? super E>)item1).compareTo(item2);
+			return ((Comparable<? super E>) item1).compareTo(item2);
 		}
 		return cmp.compare(item1, item2);
 	}
 
 	/**
-	 * Returns the number of items in this priority queue.
-	 * O(1)
+	 * Returns the number of items in this priority queue. O(1)
 	 */
 	@Override
 	public int size() {
@@ -247,8 +302,7 @@ public class BinaryMaxHeap<E> implements PriorityQueue<E> {
 	}
 
 	/**
-	 * Returns true if this priority queue is empty, false otherwise.
-	 * O(1)
+	 * Returns true if this priority queue is empty, false otherwise. O(1)
 	 */
 	@Override
 	public boolean isEmpty() {
@@ -256,8 +310,7 @@ public class BinaryMaxHeap<E> implements PriorityQueue<E> {
 	}
 
 	/**
-	 * Empties this priority queue of items.
-	 * O(1)
+	 * Empties this priority queue of items. O(1)
 	 */
 	@Override
 	public void clear() {
@@ -267,32 +320,10 @@ public class BinaryMaxHeap<E> implements PriorityQueue<E> {
 	@Override
 	public Object[] toArray() {
 		Object[] returnArray = new Object[this.size];
-		for(int i = 0; i < this.size; i++) {
+		for (int i = 0; i < this.size; i++) {
 			returnArray[i] = maxHeap[i];
 		}
 		return returnArray;
 	}
-	
-// 	public static void main(String[] args) {
-// 		BinaryMaxHeap<Integer> BH = new BinaryMaxHeap<Integer>();
-// 		System.out.println( Arrays.toString( BH.toArray() ) );
-// 		BH.add(13);
-// 		System.out.println( Arrays.toString( BH.toArray() ) );
-// 		BH.add(14);
-// 		System.out.println( Arrays.toString( BH.toArray() ) );
-// 		BH.add(-2);
-// 		System.out.println( Arrays.toString( BH.toArray() ) );
-// 		BH.add(48);
-// 		System.out.println( Arrays.toString( BH.toArray() ) );
-// 		System.out.println( BH.extractMax() );
-// 		System.out.println( Arrays.toString( BH.toArray() ) );
-// 		System.out.println( BH.extractMax() );
-// 		System.out.println( Arrays.toString( BH.toArray() ) );
-// 		System.out.println( BH.extractMax() );
-// 		System.out.println( Arrays.toString( BH.toArray() ) );
-// 		System.out.println( BH.extractMax() );
-// 		System.out.println( Arrays.toString( BH.toArray() ) );	
-// 	}
-
 
 }
