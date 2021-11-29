3c52fede3d7c3f0ee51c13f8f6f3201c0856ca2a
sethpolevoi <74836049+sethpolevoi@users.noreply.github.com>
2021-11-28 19:28:47 -0700

    initial commit

---
+++ CS2420-Assign10/src/assign10/BinaryMaxHeapTest.java
@@ -0,0 +1,15 @@
+package assign10;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class BinaryMaxHeapTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+		//tested
+	}
+
+}
